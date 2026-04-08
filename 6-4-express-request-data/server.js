051cd07fd9c74e5ee54f03c2a43ca7ef5704b0ae
zainab14-48 <s202251360@kfupm.edu.sa>
2026-04-08 12:33:10 +0300

    server.js done

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -105,24 +105,47 @@ LAB SETUP INSTRUCTIONS
  */
 
 //import express
+import express from "express";
 
 
 // create express app instance to create web server
 
+const app = express();
 
 
 // Query params: /echo?name=Ali&age=22
+app.get("/echo", (req,res)=>{
+  const {name, age} = req.query;
 
+  if (!name || !age) {
+    return res.status(400).json({ ok:false, error:"name & age required" });
+  }
+
+  res.json({ ok:true, name, age, msg:`Hello ${name}, you are ${age}` });
+});
 
 // Route params: /profile/First/Last
-
+app.get("/profile/:first/:last", (req,res)=>{
+  const { first, last } = req.params;
+  res.json({ ok:true, fullName: `${first} ${last}` });
+}
 
 // Route param middleware example: /users/42
+app.param("userId", (req,res,next,userId)=>{
+  const userIdNum = Number(userId);
+  if (isNaN(userIdNum) || userIdNum <= 0) {
+    return res.status(400).json({ ok:false, error:"userId must be positive number" });
+  }
+  req.userIdNum = userIdNum;
+  next();
+});
 
 
 // Route params: /users/:userId route
-
+app.get("/users/:userId", (req,res)=>{
+  res.json({ ok:true, userId: req.userIdNum });
+});
 
 // Start the server by listening
-
+app.listen(3000, ()=> console.log("API running at http://localhost:3000"));
 
